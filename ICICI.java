04116efd1de317286172b9bf8fe0507b75ed4aa0
Atanu-Sarkar <atanu.noida@gmail.com>
2016-04-22 00:50:13 +0530

    Bank Rate of Interest
    Java Session 5 Assignment 4

---
+++ ICICI.java
@@ -0,0 +1,7 @@
+package com.acadgild.session.five;
+
+public class ICICI extends Bank {
+	int getRateOfInterest() {
+		return 7;
+	}
+}
---
+++ SBI.java
@@ -0,0 +1,7 @@
+package com.acadgild.session.five;
+
+public class SBI extends Bank {
+	int getRateOfInterest() {
+		return 8;
+	}
+}
